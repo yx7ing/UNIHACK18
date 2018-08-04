c48089dbb88c77f875b66980e39886f932461c95
Chuangy <colin.huangy@hotmail.com>
2018-08-04 21:25:02 +1000

    Updated HTML, CSS

--- static/js/main.js
+++ static/js/main.js
@@ -4,13 +4,23 @@ $(document).ready(function() {
 })
 
 $(document).on("click", "#nav_quests", function() {
-	console.log("Hello");
-	$("#status_menu").fadeOut(300);
+	fadeOutAll(300);
 	$("#quests_menu").fadeIn(300);
 })
 
-$(document).on("click", "#nav_status", function() {
-	console.log("Hello");
-	$("#quests_menu").fadeOut(300);
+$(document).on("click", "#nav_eduquest", function() {
+	fadeOutAll(300);
 	$("#status_menu").fadeIn(300);
 })
+
+$(document).on("click", "#quests_menu li", function() {
+	fadeOutAll(300);
+	$("#quests_grid").fadeIn(300);
+	$("#quests_grid h1").text($(this).find("a").text());
+})
+
+function fadeOutAll(time) {
+	$("#status_menu").fadeOut(time);
+	$("#quests_menu").fadeOut(time);
+	$("#quests_grid").fadeOut(time);
+}
