382a3b3c7f63a235c1a5e84f44ab19a7668d438f
Samuel Lin <samlin@samuels-mbp.ait-sydney.net.au>
2018-08-05 10:15:31 +1000

    main file

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import random
+import ind as I
+import fin as F
+import lin as L
 import psycopg2
 
 app = Flask(__name__)
@@ -82,12 +86,12 @@ def get_player():
 def get_quests():
 	quests_dict = {}
 	cur.execute("""SELECT * FROM Quests""")
-    quests_dict['quest_id'] = []
+	quests_dict['quest_id'] = []
 	quests_dict['topic'] = []
 	quests_dict['subtopic'] = []
-    all = cur.fetchall()
-    for one in all:
-        quests_dict['quest_id'].append(one[0])
+	all = cur.fetchall()
+	for one in all:
+		quests_dict['quest_id'].append(one[0])
 		quests_dict['topic'].append(one[1])
 		quests_dict['subtopic'].append(one[2])
 	return jsonify(quests_dict)
@@ -95,10 +99,54 @@ def get_quests():
 def get_class():
 	class_dict = {}
 	cur.execute("""SELECT * FROM Class""")
-    class_dict['player_id'] = []
+	class_dict['player_id'] = []
 	class_dict['class_id'] = []
-    all = cur.fetchall()
-    for one in all:
-        class_dict['player_id'].append(one[0])
+	all = cur.fetchall()
+	for one in all:
+		class_dict['player_id'].append(one[0])
 		class_dict['class_id'].append(one[1])
 	return jsonify(class_dict)
+
+@app.route("/add1")
+def basic():
+    a = random.randint(0, 10)
+    b = random.randint(0, 10)
+    ans = a + b
+
+    return jsonify({"type": "add", "params": str(a) + "," + str(b) + "," + str(ans)})
+
+@app.route("/available_routes")
+def get_routes():
+    return jsonify({"routes": ["ind1", "ind2", "ind3", "fin1", "fin2", "fin3", "lin1", "lin2"]})
+
+@app.route("/ind1")
+def ind_1():
+    return I.ind_1()
+
+@app.route("/ind2")
+def ind_2():
+    return I.ind_2()
+
+@app.route("/ind3")
+def ind_3():
+    return I.ind_3()
+
+@app.route("/fin1")
+def fin_1():
+    return F.fin_1()
+
+@app.route("/fin2")
+def fin_2():
+    return F.fin_2()
+
+@app.route("/fin3")
+def fin_3():
+    return F.fin_3()
+
+@app.route("/lin1")
+def lin_1():
+    return L.gradients()
+
+@app.route("/lin2")
+def lin_2():
+    return L.equations()
